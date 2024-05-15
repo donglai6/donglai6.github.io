d963e57adb3239dc002ae565090d106f4cfb4513
donglai6 <69284599+donglai6@users.noreply.github.com>
2024-05-15 13:49:43 +0800

    Add files via upload

---
+++ schedule-temp.php
@@ -0,0 +1,263 @@
+<li> *Monday, :Friday, -Sat, -Sunday <li> [e], out of town entire
+day;  [m], out morning; [a], out afternoon  <li>  [h], holiday or
+on leave; [b], in town but blocked <li> L: lunch, P: Party, T:telecon
+</li> 
+<i> I am not fond of emails.  If you need to contact me: call
+call Gita Patel +1 626 395 4169 or gp@astro.caltech.edu 
+<br> 
+<a href="http://www.astro.caltech.edu/~srk"> Return
+to homepage</a> &nbsp &nbsp <a
+href="http://www.astro.caltech.edu/~srk/srk.plan.2024.html">2024
+Schedule (past) </a> &nbsp &nbsp <a href="srk_plan/srk.plan.2023.html">
+Previous Year </a> </i> 
+<hr>
+<pre>
+May 10:  [e] return            5p/Gemma
+May 11-[e]   lv to Berkeley
+May 12-[e]   
+May 13*[e]   McKee conference, UCB
+May 14 [e]   McKee "
+May 15 [e]   McKee 
+May 16 [e]   Stanford ISM workshop
+May 17:[e]   "
+May 18-[e]
+May 19-[e]   return
+May 20*
+May 21            2p/Annie
+May 22 [a] lv to OVRO   7p/Carver Mead
+May 23 [e] OVRO
+May 24:[a] return
+May 25-
+May 26-
+May 27*
+May 28
+May 29
+May 30    
+May 31:   last day of classes
+
+Jun  1-[]e lv to Las Cruces, Tucson 
+Jun  2-[e]   Las Cruces
+Jun  3*[e]   SDSS, Las Cruces
+Jun  4 [e]   "
+Jun  5 [e]   "
+Jun  6 [e]   "   lv to Socoro
+Jun  7:[e]   Socorro              
+Jun  8-[e]   Socorro
+Jun  9-[e]   lv to Boston
+Jun 10*[e] Kavli Meeting  
+Jun 11 [e] Narayan-50, Cambridge
+Jun 12 [e] "
+Jun 13 [e] "
+Jun 14:[e] MIT
+Jun 15-[e]
+Jun 16-[e]
+Jun 17*[e] 
+Jun 18 [e] return 
+Jun 19
+Jun 20
+Jun 21:
+Jun 22-
+Jun 23-
+Jun 24*     10a/Taylor    1p/Ky
+Jun 25
+Jun 26
+Jun 27
+Jun 28:
+Jun 29-
+Jun 30-
+
+Jul  1*
+Jul  2
+Jul  3
+Jul  4
+Jul  5:
+Jul  6-
+Jul  7-
+Jul  8*
+Jul  9
+Jul 10
+Jul 11
+Jul 12:
+Jul 13-
+Jul 14-
+Jul 15*
+Jul 16
+Jul 17
+Jul 18
+Jul 19:
+Jul 20-
+Jul 21-
+Jul 22*
+Jul 23
+Jul 24
+Jul 25
+Jul 26:
+Jul 27-
+Jul 28-
+Jul 29*
+Jul 30
+Jul 31
+
+Aug  1
+Aug  2:
+Aug  3-
+Aug  4-
+Aug  5*
+Aug  6
+Aug  7
+Aug  8
+Aug  9:
+Aug 10-
+Aug 11-
+Aug 12*
+Aug 13
+Aug 14
+Aug 15
+Aug 16:
+Aug 17-
+Aug 18-
+Aug 19*
+Aug 20
+Aug 21
+Aug 22
+Aug 23:
+Aug 24-
+Aug 25-
+Aug 26*
+Aug 27
+Aug 28
+Aug 29
+Aug 30:
+Aug 31-
+
+Sep  1-
+Sep  2*
+Sep  3
+Sep  4  Marsh, Warwick
+Sep  5  "
+Sep  6: "
+Sep  7-
+Sep  8-
+Sep  9*
+Sep 10
+Sep 11
+Sep 12
+Sep 13:
+Sep 14-
+Sep 15-
+Sep 16* SRG/eROSITA, Munich
+Sep 17  "
+Sep 18  "
+Sep 19  "
+Sep 20: "
+Sep 21-
+Sep 22-
+Sep 23*
+Sep 24
+Sep 25
+Sep 26
+Sep 27:
+Sep 28-
+Sep 29-
+Sep 30*
+
+Oct  1
+Oct  2
+Oct  3
+Oct  4:
+Oct  5-
+Oct  6-
+Oct  7*
+Oct  8
+Oct  9
+Oct 10
+Oct 11:
+Oct 12-
+Oct 13-
+Oct 14*
+Oct 15
+Oct 16
+Oct 17
+Oct 18:
+Oct 19-
+Oct 20-
+Oct 21*
+Oct 22
+Oct 23
+Oct 24
+Oct 25:
+Oct 26-
+Oct 27-
+Oct 28*
+Oct 29
+Oct 30
+Oct 31
+
+Nov  1:
+Nov  2-
+Nov  3-
+Nov  4*
+Nov  5
+Nov  6
+Nov  7
+Nov  8
+Nov  9
+Nov 10
+Nov 11*
+Nov 12
+Nov 13
+Nov 14
+Nov 15
+Nov 16
+Nov 17
+Nov 18*    KISS
+Nov 19     "
+Nov 20     "
+Nov 21     "
+Nov 22:    "
+Nov 23-
+Nov 24-
+Nov 25*
+Nov 26
+Nov 27
+Nov 28
+Nov 29
+Nov 30
+
+Dec  1
+Dec  2*
+Dec  3
+Dec  4
+Dec  5
+Dec  6
+Dec  7
+Dec  8
+Dec  9*
+Dec 10
+Dec 11
+Dec 12
+Dec 13
+Dec 14
+Dec 15
+Dec 16*
+Dec 17
+Dec 18
+Dec 19
+Dec 20
+Dec 21
+Dec 22
+Dec 23*
+Dec 24
+Dec 25
+Dec 26
+Dec 27
+Dec 28
+Dec 29
+Dec 30*
+Dec 31
+</pre>
+Mar 24*  ZTF Celebration meeting (all week)
+Mar 25
+Mar 26
+Mar 27
+Mar 28:
